89c044e7dead2e08e477fd4822a3bc2edd695282
ShaunFerns <shaun.ferns@tudublin.ie>
2020-04-29 12:11:40 +0100

    1st commit

---
+++ Assets/_Scripts/PlayerInventory.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class PlayerInventory : MonoBehaviour
+{
+    private PlayerInventoryDisplay playerInventoryDisplay;
+    private List<PickUp> inventory = new List<PickUp>();
+
+    void Awake()
+    {
+        playerInventoryDisplay = GetComponent<PlayerInventoryDisplay>();
+    }
+
+    void Start()
+    {
+        playerInventoryDisplay.OnChangeInventory(inventory);
+    }
+
+    void OnTriggerEnter2D(Collider2D hit)
+    {
+        if (hit.CompareTag("Pickup"))
+        {
+            PickUp item = hit.GetComponent<PickUp>();
+            inventory.Add(item);
+            playerInventoryDisplay.OnChangeInventory(inventory);
+            Destroy(hit.gameObject);
+        }
+    }
+}
